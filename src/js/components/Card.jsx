cc8fb369686f0664caf63353651e6606dde944d5
Morena <morenicora@gmail.com>
2026-01-19 02:42:54 +0000

    fixed card detail

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -4,14 +4,17 @@ import Card from 'react-bootstrap/Card';
 function MyCard() {
   return (
     <Card className="text-center" style={{ width: '18rem', }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src="holder.js/500px325" />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex blanditiis fuga ratione magnam magni fugit eligendi quos at dolorum labore.
         </Card.Text>
-        <Button variant="primary">Find Out More!</Button>
-      </Card.Body>
+          </Card.Body>
+        <Card.Footer>
+       <Button variant="primary">Find Out More!</Button>
+      </Card.Footer>
+    
     </Card>
   );
 }
